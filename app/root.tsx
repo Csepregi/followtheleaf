35acb7d7bdd9bb7e247959411dc82adb9631e2b8
Gabor Csepregi <gabor.csepregi@accenture.com>
2023-02-27 15:20:58 +0100

    change root meta title from remix to follow the leaf

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Follow The Leaf",
   viewport: "width=device-width,initial-scale=1",
 });
 
