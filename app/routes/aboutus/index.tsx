b55ef70d56b5806de22dd15f71ad41a3abc2d82f
gabor.csepregi <gabor.csepregi@accenture.com>
2023-08-16 23:27:28 +0200

    change about us page

--- app/routes/aboutus/index.tsx
+++ app/routes/aboutus/index.tsx
@@ -27,46 +27,37 @@ const features = [
 export default function AboutUs() {
   let location = useLocation();
   return (
-    <div className="min-h-screen w-full bg-intro-back bg-cover bg-center object-cover py-32 py-24">
+    <div className="min-h-screen w-full  object-cover py-32 py-24">
       {location.pathname === "/aboutus" && <Navbar />}
-      <section className="bg-gray-800 text-gray-100">
-        <div className="container mx-auto max-w-xl space-y-24 p-6 py-12 desktop:max-w-7xl desktop:px-8">
-          <div className="grid desktop:grid-cols-2 desktop:items-center desktop:gap-8">
-            <div>
-              <div className="mt-12 space-y-12">
-                {features.map((feature) => (
-                  <div key={feature.name} className="flex">
-                    <div className="flex-shrink-0">
-                      <div className="bg-violet-400 text-gray-900 flex h-12 w-12 items-center justify-center rounded-md">
-                        <img
-                          className="h-6 w-max laptop:h-10"
-                          src="./Logo.png"
-                          alt="logo"
-                        />
-                      </div>
-                    </div>
-                    <div className="ml-4">
-                      <h4 className="text-lg text-gray-50 font-medium leading-6">
-                        {feature.name}
-                      </h4>
-                      <p className="text-gray-400 mt-2">
-                        {feature.description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div aria-hidden="true" className="mt-10 desktop:mt-0">
-              <img
-                src="https://source.unsplash.com/random/400x650/?sicily"
-                alt=""
-                className="bg-gray-500 mx-auto rounded-lg shadow-lg"
-              />
-            </div>
-          </div>
+      <section className="text-gray-600 body-font">
+  <div className="container px-5 py-6 mx-auto">
+    
+    <div className="flex flex-wrap -m-4 -mx-4 -mb-10 -mt-4 tablet:space-y-0 space-y-2">
+    {features.map((feature) => (
+      <div key={feature.name}  className="p-4 tablet:w-1/2 flex">
+        {/* <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-6 h-6" viewBox="0 0 24 24">
+            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            
+          </svg>
+        </div> */}
+        <div className="flex-grow pl-6">
+          <h2 className="text-gray-900 text-lg title-font font-medium mb-2">{feature.name}</h2>
+          <p className="leading-relaxed text-base"> {feature.description}</p>
         </div>
-      </section>
+      </div>
+      ))}
+     
+    </div>
+    <div aria-hidden="true" className="hidden mt-10 desktop:block mt-30">
+        <img
+          src="https://source.unsplash.com/random/400x650/?sicily"
+          alt=""
+          className="bg-gray-500 mx-auto rounded-lg shadow-lg"
+        />
+    </div>
+  </div>
+</section>
     </div>
   );
 }
