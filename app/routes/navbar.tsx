d011a927a77aaa4a6008ec62b3ccfc1002a8cd30
gabor.csepregi <gabor.csepregi@accenture.com>
2023-10-01 23:27:42 +0200

    header style bugfix

--- app/routes/navbar.tsx
+++ app/routes/navbar.tsx
@@ -54,8 +54,8 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center  tablet:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+              <div className="flex flex-1 items-center justify-center ">
+                <div className="flex flex-shrink-0 items-center absolute tablet:left-0">
                   <img
                     className="stroke-zinc-950 h-16 w-auto "
                     src="/logoLeaf.png"
@@ -79,30 +79,29 @@ export default function Navbar() {
                     ))}
                   </div>
                 </div>
-                <div className="flex-auto">
-  <div className="flex justify-end items-center">
-    <div className="flex gap-2 ">
-      <div className="inline-flex items-center pt-2"> 
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-8">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-</svg>
-
-      </div>
-      <div className="inline-flex items-center">
-        <div className="w-full">
-          <input id="default-radio-1" type="radio" value="" name="default-radio" className="peer opacity-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-          <label htmlFor="default-radio-1" className="flex cursor-pointer bg-gray-200 h-10 w-full peer-checked:bg-rose-500 peer-checked:text-green text-[17px] text-sm font-medium text-gray-900 dark:text-gray-300" onClick={() => changeLanguage("it")}>it</label>
-        </div>
-      </div>
-      <div className="inline-flex items-center">
-        <div className="w-full">
-          <input id="default-radio-2" type="radio" value="" name="default-radio" className="peer opacity-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-          <label htmlFor="default-radio-2" className="flex cursor-pointer bg-gray-200 h-10 w-full peer-checked:bg-rose-500 peer-checked:text-green text-[17px] text-sm font-medium text-gray-900 dark:text-gray-300" onClick={() => changeLanguage("en")}>en</label>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> 
+                <div className="absolute inset-y-0 right-0 ">
+                  <div className="flex justify-end items-center">
+                    <div className="flex gap-2 ">
+                      <div className="inline-flex items-center pt-2"> 
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+                </svg>
+                      </div>
+                      <div className="inline-flex items-center">
+                        <div className="w-full">
+                          <input id="default-radio-1" type="radio" value="" name="default-radio" className="peer opacity-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                          <label htmlFor="default-radio-1" className="flex cursor-pointer bg-gray-200 h-10 w-full peer-checked:bg-rose-500 peer-checked:text-green text-[17px] text-sm font-medium text-gray-900 dark:text-gray-300" onClick={() => changeLanguage("it")}>it</label>
+                        </div>
+                      </div>
+                      <div className="inline-flex items-center">
+                        <div className="w-full">
+                          <input id="default-radio-2" type="radio" value="" name="default-radio" className="peer opacity-0 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                          <label htmlFor="default-radio-2" className="flex cursor-pointer bg-gray-200 h-10 w-full peer-checked:bg-rose-500 peer-checked:text-green text-[17px] text-sm font-medium text-gray-900 dark:text-gray-300" onClick={() => changeLanguage("en")}>en</label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
 
 
 
